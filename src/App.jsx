8c991e29ee4ea306f17c69e6fd3de2336fdb2ebf
Gaurav <99120502+GauravAwasare19@users.noreply.github.com>
2022-12-16 22:56:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Header from './Header'
+import MemeC from './MemeC'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+    <MemeC />
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Header(){
+    return(
+        <header className="header">
+            <img className="header--image"
+            src="./troll-face.png"/>
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
